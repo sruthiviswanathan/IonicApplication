abf4cf30b51d8fb50ca70561e0d7d8f1d2ff1e20
Sruthi Viswanathan <sravan1097@gmail.com>
2020-03-03 23:45:08 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { timer } from 'rxjs/observable/timer';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  showSplash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -19,9 +22,16 @@ export class AppComponent {
   }
 
   initializeApp() {
+    this.splashScreen.show();
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      // Setting Timer for Splash screen
+      timer(3000).subscribe(() => {
+        this.showSplash = false;
+      })
+
     });
   }
 }
