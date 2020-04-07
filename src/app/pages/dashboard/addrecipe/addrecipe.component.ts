d02d20660fa325b89c12a94be02acf6271bae52d
sruthiviswanathan <31382544+sruthiviswanathan@users.noreply.github.com>
2020-04-07 22:50:28 +0530

    Added action sheet to delete images.

--- src/app/pages/dashboard/addrecipe/addrecipe.component.ts
+++ src/app/pages/dashboard/addrecipe/addrecipe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { PhotoService } from '../../../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-addrecipe',
   templateUrl: './addrecipe.component.html',
@@ -11,7 +13,7 @@ export class AddrecipeComponent implements OnInit {
   photos: any;
 
   constructor(private modalController: ModalController,
-    public photoService: PhotoService) { }
+    public photoService: PhotoService, private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
     
@@ -30,6 +32,28 @@ export class AddrecipeComponent implements OnInit {
   }
   takeAPicture() {
     this.photoService.takeNewPhotoAndAddToGallery();
+  }
+
+  public async showActionSheet(photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
