6ed11f6b7aebca42961ac8762663b9a5e9e3202d
sruthiviswanathan <31382544+sruthiviswanathan@users.noreply.github.com>
2020-04-10 15:46:14 +0530

    moved common-header to shared component.

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -10,7 +10,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 
 
 import { DashboardPage } from './dashboard.page';
-import {CommonheaderComponent} from '../../components/commonheader/commonheader.component';
+import {CommonheaderComponent} from '../../shared/commonheader/commonheader.component';
 import { RecipeComponent} from '../dashboard/recipe/recipe.component'
 import { AddrecipeComponent } from './addrecipe/addrecipe.component';
 
