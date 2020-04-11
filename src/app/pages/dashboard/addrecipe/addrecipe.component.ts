38544432a195067d7fc47ab067b4e8a23b7ddf28
sruthiviswanathan <31382544+sruthiviswanathan@users.noreply.github.com>
2020-04-11 19:26:22 +0530

    Image delete option completed.

--- src/app/pages/dashboard/addrecipe/addrecipe.component.ts
+++ src/app/pages/dashboard/addrecipe/addrecipe.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, AlertController } from '@ionic/angular';
 import { PhotoService } from '../../../services/photo.service';
-import { ActionSheetController } from '@ionic/angular';
 
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireStorage, AngularFireUploadTask, AngularFireStorageReference } from 'angularfire2/storage';
@@ -10,7 +9,8 @@ import { NgForm } from '@angular/forms';
 import { Recipe } from '../../../models/recipe';
 import { ToastService } from '../../../shared/services/toast.service';
 import { Observable } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
+import { recipeImage } from 'src/app/models/recipeImage';
 
 @Component({
   selector: 'app-addrecipe',
@@ -19,6 +19,7 @@ import { finalize, tap } from 'rxjs/operators';
 })
 export class AddrecipeComponent implements OnInit {
 
+  recipeImageObject: Array<recipeImage> = [];
   recipeImages: string[] = [];
   tagNames: string[];
   photos: any;
@@ -48,17 +49,23 @@ export class AddrecipeComponent implements OnInit {
   
   async addNewRecipe(form: NgForm) {
     if (form.valid) {
-      if (this.recipeImages.length != 0) {
+      if (this.recipeImageObject.length != 0) {
         let finalTags: Array<any> = [];
         for(let i=0; i< this.tagNames.length ; i++) {
           finalTags.push(this.tagNames[i].trim().toLowerCase());
-        } 
-        const newRecipe: Recipe = new Recipe(form.value, this.recipeImages, finalTags);
+        }
+        let finalImages: Array<any> = [];
+        for (let i=0; i< this.recipeImageObject.length; i++) {
+          finalImages.push(this.recipeImageObject[i].imageUrl);
+        }
+        const newRecipe: Recipe = new Recipe(form.value, finalImages, finalTags);
         this.fireStore.collection('recipes').doc(newRecipe.id).set({...newRecipe})
         .then(response => {
           this.toastService.successToast('Successfully added your recipe!!');
           this.tagNames = [];
           this.recipeImages = [];
+          this.recipeImageObject = [];
+          this.uploader.queue = [];
           form.resetForm();
         }).catch(error => {
           this.toastService.errorToast(error.message);
@@ -67,14 +74,16 @@ export class AddrecipeComponent implements OnInit {
           this.alertPopUp('OOPS! Something went wrong', 'Upload at least one image');
       }
     } else {
-      this.alertPopUp('OOPS! Something went wrong','Please fill out all the fields');
+          this.alertPopUp('OOPS! Something went wrong','Please fill out all the fields');
     }
   }
 
 
   async uploadImage() {
-    this.showProgress = true; 
-    const file = this.getFiles()[0].rawFile;
+    const currentImageIndex = this.uploader.queue.length - 1;
+    this.showProgress = true;
+    const file = this.getFiles()[currentImageIndex].rawFile;
+    const fileName = this.getFiles()[currentImageIndex].name;
     const id = Math.random().toString(36).substring(2);
     this.ref = this.storage.ref(id);
     this.task = this.ref.put(file);
@@ -85,9 +94,12 @@ export class AddrecipeComponent implements OnInit {
       finalize(() =>{
         this.downloadUrl = this.ref.getDownloadURL();
         this.downloadUrl.subscribe(url => {
-          console.log(url);
           this.recipeImages.push(url);
           this.showProgress = false;
+          this.recipeImageObject.push({
+            'name': fileName,
+            'imageUrl': url
+          });
         });
       })).subscribe();
   }
@@ -113,11 +125,18 @@ export class AddrecipeComponent implements OnInit {
     this.hasBaseDropZoneOver = ev;
   }
 
-  remove(fileName) {
+  remove(fileName: any) {
     let index = this.uploader.queue.indexOf(this.uploader.queue.find(fileItem => fileItem.file.name === fileName));
     if(index > -1){
       this.uploader.queue.splice(index, 1);
     }
+ 
+    for (let i=0; i < this.recipeImageObject.length ; i++) {
+      if(this.recipeImageObject[i].name == fileName ) {
+        this.recipeImageObject.splice(i, 1);
+      }
+    }
+   
   }
 
   async alertPopUp(header: string, message: string) {
